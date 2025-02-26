bf61bcbb8c65fc82e515eae6901ced9823d438f2
felipe <felipemeirelessantana@gmail.com>
2025-02-26 19:38:52 -0300

    c16 - Redis

--- src/modules/products/services/ListProductService.ts
+++ src/modules/products/services/ListProductService.ts
@@ -1,14 +1,17 @@
-/* Serviço de Listagem de Produtos */
-
 import { getCustomRepository } from 'typeorm';
 import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
 import Product from '../typeorm/entities/Product';
+import RedisCache from '@shared/cache/RedisCache';
 
 class ListProductService {
     public async execute(): Promise<Product[]> {
         const productsRepository = getCustomRepository(ProductRepository);
 
+        const redisCache = new RedisCache();
+
         const products = productsRepository.find();
+
+        await redisCache.save('teste', 'teste');
 
         return products;
     }
